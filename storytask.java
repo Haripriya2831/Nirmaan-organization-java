2835f006ae4790fb9b81e31bcd7cb9b987417736
Haripriya2831 <haripriyavijayan2831@gmail.com>
2024-11-28 12:53:54 +0530

    Day2

---
+++ storytask.java
@@ -0,0 +1,25 @@
+public class storytask
+{
+public static void main(String agrs[])
+{
+String name1 ="Haripriya";
+String adjective1 ="happy";
+String adjective2 ="sadness";
+String noun1 ="fishes"; 
+String noun2 ="net";
+String verb1 ="chocolate";
+String noun3 ="jump";
+String noun4 ="pinao";
+String adjective3 ="children";
+String name2 ="hariharan";
+String place1 ="lakhak";
+String noun5 ="ice cream";
+String noun6 ="robot";
+int number = 2026;
+
+    String storytask = "This morning " + name1 + " woke up feeling " + adjective1 + ". 'It is going to be a " + adjective2+ " day!' Outside, a bunch of " + noun1+ "s were protesting to keep " + noun2 + " in stores. They began to " + verb1 + " to the rhythm of the " + noun3 + ", which made all the " + noun4 + "s very " + adjective3 + ".Concerned, " + name1 + " texted " + name2 + ", who flew " + name1 + " to " + place1 + " and dropped " +name1 + " in a puddle of frozen "+noun5 + ". " + name1 + " woke up in the year " + number + ", in a world where " + noun6 + "s ruled the world.";
+ { 
+ System.out.println(storytask);
+}
+}
+}
