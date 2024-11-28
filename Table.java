6e3dbae0b165ee562bd523f861ad05f212e02ca1
Haripriya2831 <haripriyavijayan2831@gmail.com>
2024-11-28 13:07:47 +0530

    day6 part1

---
+++ Table.java
@@ -0,0 +1,44 @@
+package day6;
+
+public class Table {
+public static void main(String args[]) {
+	int i=1;
+//for (i=1;i<=100;i++)
+//{
+//	if(i%5==0) {
+//		System.out.println("Haripriya");
+//	}
+//else {
+//	System.out.println(i);
+//}
+//}}
+//}
+
+
+
+
+	for(i=1;i<=100;i++)
+	{
+		if(i%2==0) {
+			System.out.println("Even");
+		}
+		else {
+			System.out.println(i);
+		}
+			}
+}
+
+}
+
+
+
+for(i=1;i<=100;i++)
+	{
+	if(i%2!==0) {
+		System.out.println("Odd");
+	}
+	else {
+		System.out.println(i);
+	}
+	}}
+	}
